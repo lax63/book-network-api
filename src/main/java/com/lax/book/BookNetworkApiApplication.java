bf9219bb6294ef58b38263fce859679ca7729678
lax63 <40056993+lax63@users.noreply.github.com>
2024-04-30 22:51:07 +0530

    jwt-service (#1)

--- src/main/java/com/lax/book/BookNetworkApiApplication.java
+++ src/main/java/com/lax/book/BookNetworkApiApplication.java
@@ -1,13 +1,34 @@
 package com.lax.book;
 
+import com.lax.book.entity.Role;
+import com.lax.book.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class BookNetworkApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookNetworkApiApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
+
+
+    @Bean
+    public static CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(
+                        Role.builder().name("USER").build()
+                );
+            }
+        };
+    }
 
 }
